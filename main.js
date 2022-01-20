83b7ff0f9203c54c804e172fc1caffa1bad795a2
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-01-20 17:27:57 +0200

    working on index.html

---
+++ main.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems, {});
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+const elems = document.querySelectorAll('.chips');
+const instances = M.Chips.init(elems, {});
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.datepicker');
+var instances = M.Datepicker.init(elems, {});
+});
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.timepicker');
+var instances = M.Timepicker.init(elems, {});
+});
